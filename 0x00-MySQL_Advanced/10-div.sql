59f060a0204d4c2314229c2d42310d42bff21831
root <y.ishimwe2@alustudent.com>
2023-07-12 06:27:12 -0700

    Safe Divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,4 +1,4 @@
--- creates a function SafeDiv that divides (and returns) the first
+-- creates a function SafeDiv that divides (and returns) the first,
 -- by the second number or returns 0 if the second number is equal to 0.
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$

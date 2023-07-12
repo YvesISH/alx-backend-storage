0376f8d153039a804934573544feb0a9c9507075
root <y.ishimwe2@alustudent.com>
2023-07-12 00:51:44 -0700

    Buy Buy Buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
 -- Creates a trigger that decreases the quality
--- of an item after adding a new order
+-- of an item after adding a new order.
 DROP TRIGGER IF EXISTS reduce_quantity;
 DELIMITER $$
 CREATE TRIGGER reduce_quantity

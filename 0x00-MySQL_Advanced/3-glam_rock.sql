fcb13b81c3cf789a7c91769afc6bf8ba86a0e7e0
root <y.ishimwe2@alustudent.com>
2023-07-13 00:47:06 -0700

    Old School Band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands

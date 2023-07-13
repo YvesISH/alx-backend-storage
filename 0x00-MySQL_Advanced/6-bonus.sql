f2c4aa768a7aa9598acb1819ff43bcc5cfe3ebe7
root <y.ishimwe2@alustudent.com>
2023-07-13 00:43:15 -0700

    Add Bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,5 +1,5 @@
 -- Creates a stored procedure AddBonus that adds a new
--- correction for a student .
+-- correction for a student.
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
 CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
